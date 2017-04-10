8c6ec14c9c6ef4cd5be2c7e75e1d6ce42b0aa9d3
ParadiseCatz <ChristianAnthonySetyawan@gmail.com>
2017-04-10 21:00:27 +0700

    Add 2d Dragon

--- Grafika/main.cpp
+++ Grafika/main.cpp
@@ -3,7 +3,6 @@
 #include</Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.12.sdk/System/Library/Frameworks/GLUT.framework/Headers/glut.h>
 #include<stdio.h>
 #include <cstdlib>
-#include <vector>
 //globals
 GLuint elephant;
 float elephantrot;
@@ -11,10 +10,7 @@ char ch='1';
 //other functions and main
 //wavefront .obj loader code begins
 
-std::vector<GLfloat> vertices;
 
-// This is the identifier for your vertex buffer
-GLuint vbo;
 void loadObj(char *fname)
 {
     FILE *fp;
@@ -32,29 +28,42 @@ void loadObj(char *fname)
     glNewList(elephant, GL_COMPILE);
     {
         glPushMatrix();
-        glBegin(GL_POINTS);
-        while(!(feof(fp)))
-        {
-            read=fscanf(fp,"%c %f %f %f",&ch,&x,&y,&z);
-            if(read==4&&ch=='v')
-            {
-                glVertex3f(x,y,z);
-                vertices.push_back(x);
-                vertices.push_back(y);
-                vertices.push_back(z);
-                vertices.push_back(1.0);
-                
-            }
-        }
+        glBegin(GL_POLYGON);
+        glVertex3f(315.2,548.3, -5.0f);
+        glVertex3f(392,545.9, -5.0f);
+        glVertex3f(394.6,603.2, -5.0f);
+//        glVertex3f(409.9,551.2, -5.0f);
+        glVertex3f(393.9,545.2, -5.0f);
+        glVertex3f(427.7,542.7, -5.0f);
+        glVertex3f(543.4,522.1, -5.0f);
+        glVertex3f(579.8,482.1, -5.0f);
+        glVertex3f(508.5,505.8, -5.0f);
+        glVertex3f(529.6,516.1, -5.0f);
+        glVertex3f(430.4,499.2, -5.0f);
+        glVertex3f(430.6,480.3, -5.0f);
+        glVertex3f(457.4,401.7, -5.0f);
+        glVertex3f(520,342.8, -5.0f);
+        glVertex3f(373.8,338.4, -5.0f);
+        glVertex3f(347.3,437.7, -5.0f);
+        glVertex3f(296.4,349.9, -5.0f);
+        glVertex3f(274.9,361.2, -5.0f);
+        glVertex3f(262.3,437.4, -5.0f);
+//        glVertex3f(279.8,462.5, -5.0f);
+        glVertex3f(329.5,500.7, -5.0f);
+        glVertex3f(185.4,500.2, -5.0f);
+        glVertex3f(188.4,471.9, -5.0f);
+        glVertex3f(171.2,503.7, -5.0f);
+        glVertex3f(152.2,481.7, -5.0f);
+        glVertex3f(156.1,509.7, -5.0f);
+        glVertex3f(139.4,525.3, -5.0f);
+        glVertex3f(163,531.6, -5.0f);
+        glVertex3f(151.5,549, -5.0f);
+        glVertex3f(189.6,537, -5.0f);
+        glVertex3f(303.1,548.5, -5.0f);
+        glVertex3f(312.7,587.3, -5.0f);
+        glVertex3f(325.9,553.3, -5.0f);
         glEnd();
     }
-
-    // This creates our identifier and puts it in vbo
-    glGenBuffers(1, &vbo);
-    // This binds our vbo
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    // This hands the vertices into the vbo and to the rendering pipeline
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
     glPopMatrix();
     glEndList();
     fclose(fp);
@@ -72,10 +81,11 @@ void reshape(int w,int h)
 void drawElephant()
 {
     glPushMatrix();
-    glTranslatef(0,-40.00,-105);
+    glTranslatef(-60,80.00,-105);
     glColor3f(1.0,0.23,0.27);
-    glScalef(10,10,10);
-    glRotatef(elephantrot,0,1,0);
+    glScalef(0.2,0.2,0.2);
+//    glRotatef(elephantrot,0,1,0);
+    glRotatef(180,1,0,0);
     glCallList(elephant);
     glPopMatrix();
     elephantrot=elephantrot+0.6;
@@ -88,19 +98,6 @@ void display(void)
     glLoadIdentity();
     drawElephant();
     glutSwapBuffers(); //swap the buffers
-    // "Enable a port" to the shader pipeline
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    // pass information about how vertex array is composed
-    glVertexAttribPointer(0, // same as in glEnableVertexAttribArray(0)
-                          4, // # of coordinates that build a vertex
-                          GL_FLOAT, // data type
-                          GL_FALSE, // normalized?
-                          0,        // stride
-                          (void*)0);// vbo offset
-    
-    glDrawArrays(GL_LINES, 0, 2);
-    glDisableVertexAttribArray(0);
 }
 int main(int argc,char **argv)
 {
