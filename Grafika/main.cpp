60a8ee10d8ffa521df65caf27a606da11501d559
ParadiseCatz <ChristianAnthonySetyawan@gmail.com>
2017-04-10 16:52:22 +0700

    Initial Commit

---
+++ Grafika/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Grafika
+//
+//  Created by Anthony Setyawan on 4/10/17.
+//  Copyright © 2017 Anthony Setyawan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
